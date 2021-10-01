a3b3cbf3db17c4de6217e83abe496a8069b8e19f
김지은 <kimmjieun@pincar.co.kr>
2021-10-01 11:52:28 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,41 +1,31 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-//@Entity(name = "USER") // 테이블과 매핑이 가능함 가급적 기본값
 public class Member {
-
     @Id
     private Long id;
 
-    //@Column(name = "username") // 컬럼명 매핑 가능함
-    private String name;
+    @Column(name = "name") // 객체는 username, 컬럼명은 name
+    private String username;
 
-    private int age;
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob // 큰 콘텐츠 디비의 TEXT 비슷
+    private String description;
+
     public Member(){
-    }
 
-    public Member(Long id, String name){
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
     }
 }
