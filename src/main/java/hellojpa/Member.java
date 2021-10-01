404a59a5441f5851775b8c29f3d2d8e4c8081011
김지은 <kimmjieun@pincar.co.kr>
2021-10-01 10:26:02 +0900

    데이터베이스 스키마 자동생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-//@Entity(name = "USER") // 테이블과 매핑이 가능함
+//@Entity(name = "USER") // 테이블과 매핑이 가능함 가급적 기본값
 public class Member {
 
     @Id
@@ -14,6 +14,14 @@ public class Member {
     //@Column(name = "username") // 컬럼명 매핑 가능함
     private String name;
 
+    private int age;
+    public Member(){
+    }
+
+    public Member(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId(){
         return id;
