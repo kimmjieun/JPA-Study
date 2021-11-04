50dee5d59623e04da8e1bf7640a9179dc8d77695
김지은 <kimmjieun@gmail.com>
2021-11-04 17:20:43 +0900

    영속성 전이(CASCADE)와 고아 객체

---
+++ src/main/java/hellojpa/Child.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Child {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="parent_id")
+    private Parent parent;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+    }
+}
