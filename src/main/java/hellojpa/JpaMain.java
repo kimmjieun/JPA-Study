b8948dbd387e8e9d81641da2d4a282e625819afc
김지은 <kimmjieun@gmail.com>
2021-11-08 11:00:44 +0900

    값 타입 컬렉션

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -14,16 +15,48 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Address address = new Address("city","street","zipcode");
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setAddress(address);
+            member.setAddress(new Address("homecity","street","zipcode"));
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street1", "zipcode1"));
+            member.getAddressHistory().add(new AddressEntity("old2", " street2", "zipcode2"));
+
             em.persist(member); // 디비에 값 반영
 
+            em.flush();
+            em.clear();
 
-            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
-            member.setAddress(newAddress);
+            System.out.println("=======START=======");
+            Member findMember = em.find(Member.class, member.getId());// 멤버만 가져온다 , 컬렉션은 지연로딩이다
+
+//            List<Address> addressHistory = findMember.getAddressHistory();
+//            for (Address address : addressHistory){
+//                System.out.println("address = "+ address.getCity());
+//            }
+//
+//            Set<String> favoritFoods = findMember.getFavoriteFoods();
+//            for( String favoriteFood : favoritFoods){
+//                System.out.println("favoriteFood = " + favoriteFood);
+//            }
+
+            // 값 타입 컬렉션 수정
+
+            // 치킨 -> 한식
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
+
+
+            // 주소 변경
+//            findMember.getAddressHistory().remove(new Address("old1", "street1", "zipcode1"));
+//            findMember.getAddressHistory().add(new Address("new1", "street1", "zipcode1"));
+
+
+
 
             tx.commit();
         } catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,12 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static javax.persistence.CascadeType.ALL;
 
 @Entity
 public class Member {
@@ -19,6 +25,19 @@ public class Member {
     // Address
     @Embedded
     private Address address;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade=ALL, orphanRemoval = true)
+    @JoinColumn(name = "member_id")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -50,5 +69,21 @@ public class Member {
 
     public void setAddress(Address address) {
         this.address = address;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
